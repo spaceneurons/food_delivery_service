0b51d02868847d102a777aee2edf737ae38256b1
Marogy <rushevamar@mail.ru>
2021-10-25 17:09:40 +0300

    слайдер+адаптивная верстка

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    navigation: {
+      nextEl: '.slider-button-next',
+      prevEl: '.slider-button-prev',
+    },
+  
+    
+  });
+  
